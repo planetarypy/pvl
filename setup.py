51ad976efc3dd466f413cf864beaaffd10ecd811
Ross Beyer <rbeyer@rossbeyer.net>
2021-03-27 17:18:46 -0700

    Bump version: 1.2.0-dev → 1.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='pvl',
-    version='1.2.0-dev',
+    version='1.2.0',
     description=(
         'Python implementation for PVL (Parameter Value Language) '
         'parsing and encoding.'
