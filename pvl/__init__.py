31d36be50c379e4e0b0b1a7fa92a2d4abe47f481
Ross Beyer <rbeyer@rossbeyer.net>
2022-01-12 20:56:03 -0800

    fix(version): Updated version from 1.3.0 to 1.3.1-dev

--- pvl/__init__.py
+++ pvl/__init__.py
@@ -24,7 +24,7 @@ from .collections import (
 
 __author__ = "The pvl Developers"
 __email__ = "rbeyer@rossbeyer.net"
-__version__ = "1.3.0"
+__version__ = "1.3.1-dev"
 __all__ = [
     "load",
     "loads",
